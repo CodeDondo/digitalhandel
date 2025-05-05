643d1c278c1a98b8cb2dfb753f832f9791ce45f0
JustDondo <162633910+CodeDondo@users.noreply.github.com>
2025-05-05 11:04:24 +0200

    changes

--- src/context/_CartContext.tsx
+++ src/context/_CartContext.tsx
@@ -3,8 +3,12 @@ import { ProductElement } from "../types/Product.types";
 
 export const CartContext = createContext({
     cartData: [] as Array<ProductElement>,
-    setCartData: (productArr: ProductElement[]) => { },
-    addToCart: (product: ProductElement) => { },
-    removeFromCart: (productID: number) => { },
+    setCartData: (productArr: ProductElement[]) => { console.log('Set cart data', productArr) },
+    addToCart: (product: ProductElement) => {
+        console.log('Add to cart', product)
+    },
+    removeFromCart: (productID: number) => {
+        console.log('Remove from cart', productID)
+    },
     clearCart: () => { }
 });
